4c07baa534b851e5902520e0180226b165931d03
Charles Wong <charlie-wong@outlook.com>
2024-04-11 23:29:26 +0800

    docs(demo): update HTTPS server with X.509 cert

--- source/demo/js-https/https-server.js
+++ source/demo/js-https/https-server.js
@@ -3,18 +3,23 @@
 const fs = require("fs");
 const https = require("https");
 
-// 生成证书私钥
-//   openssl genrsa -out rsa-secret.key 1024
-// 生成签名请求(Certificate Signing Request)，密码 lasa
-//   openssl req -new -key rsa-secret.key -out rsa-request.csr
+// NOTE 浏览器暂不支持 ED25519 格式证书, 使用 RSA2048 或 RSA4096 算法创建证书
+//
+// 生成私钥/公钥(不加密无密码)
+//   openssl genrsa -out rsa2048.key 2048
+// 生成签名请求(Certificate Signing Request)文件
+//   openssl req -new -key rsa2048.key -out rsa2048.csr -subj '/C=CN/O=Development/CN=Demo'
+//
+// 生成 CSR 文件和私钥/公钥(不加密无密码)
+//   openssl req -new -newkey rsa:2048 -nodes -out rsa2048.csr -keyout rsa2048.key -subj '/C=CN/O=Development/CN=Demo'
+//
 // 生成自签名证书
-//   openssl x509 -req -in rsa-request.csr -signkey rsa-secret.key -out rsa-public.cer
+//   openssl x509 -req -in rsa2048.csr -signkey rsa2048.key -out rsa2048.crt
+
 let options = {
-  key:  fs.readFileSync('./rsa-secret.key'),
-  cert: fs.readFileSync('./rsa-public.cer')
+  key:  fs.readFileSync('./rsa2048.key'),
+  cert: fs.readFileSync('./rsa2048.crt')
 };
-// 生成 CSR 文件和私钥
-//   openssl req -new -newkey rsa:2048 -nodes -out rsa-request.csr -keyout rsa-secret.key
 
 https.createServer(options, function(request, response) {
   //console.log("REQ:", request);
@@ -44,4 +49,4 @@ https.createServer(options, function(request, response) {
 console.log("Local HTTPS Server: https://localhost:5555");
 console.log("检测命令  $ curl -k https://localhost:5555");
 console.log("检测命令  $ curl -v https://localhost:5555");
-console.log("验证方式  浏览器打开 http://localhost:5555");
+console.log("验证方式  浏览器    https://localhost:5555");
